d0b119a3f0caa49b1b25a07a8266c6e4eb026d09
xehivs <pawel@ksienie.com>
2013-06-19 22:28:13 +0200

    Validator.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,7 +1,7 @@
 class Station < ActiveRecord::Base
-  validates :name,        :presence => true, :uniqueness => true
+  validates :name,        :presence => true
   validates :description, :presence => true
   validates :shortcut,    :presence => true
   validates :genre,       :presence => true
-  validates :url,         :format => URI::regexp(%w(http))
+  validates :url,         :format => URI::regexp(%w(http)), :uniqueness => true
 end
