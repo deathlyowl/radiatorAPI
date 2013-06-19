55a35090ab728148206c2e7d6a4ae319d9b33b37
xehivs <pawel@ksienie.com>
2013-06-19 20:58:24 +0200

    Validations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,7 @@
 class Station < ActiveRecord::Base
+  validates :name,        :presence => true, :uniqueness => true
+  validates :description, :presence => true
+  validates :shortcut,    :presence => true
+  validates :genre,       :presence => true
+  validates :url,         :format => URI::regexp(%w(http))
 end
