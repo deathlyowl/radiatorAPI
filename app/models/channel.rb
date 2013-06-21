a09984753631576765ecf6b42f69d6182985e38e
xehivs <pawel@ksienie.com>
2013-06-21 12:10:31 +0200

    Faster hints

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,10 +3,11 @@ class Channel < ActiveRecord::Base
   belongs_to :transmitter
   belongs_to :station
   
-  
   def hint
+    @selfName = self.name.to_ascii.downcase
     Station.all.each do |station|
-      if self.name.to_ascii.downcase.include?(station.name.to_ascii.downcase) || station.name.to_ascii.downcase.include?(self.name.to_ascii.downcase)
+      @stationName = station.name.to_ascii.downcase
+      if @selfName.include?(@stationName) || @stationName.include?(@selfName)
         return station.name
       end 
     end
